99a475f1e3120660f64eb7e889ed9b9437563639
Your Name <you@example.com>
2024-05-17 21:39:41 +0530

    add dashboard components

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,6 +23,9 @@ window.addEventListener('load',function (){
     const loadingScreen1 = document.querySelector('#loginPage');
     loadingScreen1.style.display = 'flex';
 
+    const loadingScreen2 = document.querySelector('#dashboardPage');
+    loadingScreen2.style.display = 'none';
+
 });
 
 
@@ -33,6 +36,9 @@ $(document).ready(function(){
 
       const loadingScreen2 = document.querySelector('#registerPage');
       loadingScreen2.style.display = 'block';
+
+      const loadingScreen3 = document.querySelector('#dashboardPage');
+      loadingScreen3.style.display = 'none';
    });
 
     $('#signUpBtn1').eq(0).on('click', function() {
@@ -41,5 +47,19 @@ $(document).ready(function(){
 
         const loadingScreen2 = document.querySelector('#registerPage');
         loadingScreen2.style.display = 'none';
+
+        const loadingScreen3 = document.querySelector('#dashboardPage');
+        loadingScreen3.style.display = 'none';
     });
+
+    $('#loginBtn').eq(0).on('click', function() {
+        const loadingScreen = document.querySelector('#loginPage');
+        loadingScreen.style.display = 'none';
+
+        const loadingScreen2 = document.querySelector('#registerPage');
+        loadingScreen2.style.display = 'none';
+
+        const loadingScreen3 = document.querySelector('#dashboardPage');
+        loadingScreen3.style.display = 'block';
+    })
 });
