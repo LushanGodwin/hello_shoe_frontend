f68de9a6df2e9280a7583fe9e9f5c997304c2a2a
Your Name <you@example.com>
2024-06-04 00:37:43 +0530

    done item

---
+++ model/itemModel.js
@@ -0,0 +1,10 @@
+export class ItemModel{
+    constructor(itemCode,itemDesc,pic,genderCode,occasionCode,varietyCode) {
+        this.itemCode = itemCode;
+        this.itemDesc = itemDesc;
+        this.pic=pic;
+        this.genderCode=genderCode;
+        this.occasionCode=occasionCode;
+        this.varietyCode=varietyCode;
+    }
+}
